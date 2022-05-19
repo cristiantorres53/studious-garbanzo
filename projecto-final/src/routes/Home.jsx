b9ff018fa59415d766976c34c01b167b25f5e6ec
Cristiantorres53 <cristiancamilotorresrodriguez@gmail.com>
2022-05-19 16:28:06 -0500

    creacion de rutas y componente navbar

---
+++ projecto-final/src/routes/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+
+    const saludo = "holaaaa";
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <h2>{saludo}</h2>
+    </div>
+  );
+};
+
+export default Home;

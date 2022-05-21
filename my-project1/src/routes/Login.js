1e47db986938489c4ac864540694aa4b1ca5a6cf
Cristiantorres53 <cristiancamilotorresrodriguez@gmail.com>
2022-05-21 16:25:17 -0500

    creacion de registro exitoso

--- my-project1/src/routes/Login.js
+++ my-project1/src/routes/Login.js
@@ -1,14 +1,57 @@
 import React, { useState } from "react";
+import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
-  return <div>
-      <input type="email" placeholder="ingrese el email"/>
-      <input type="password" placeholder="ingrese la contraseña"/>
-  </div>;
+
+  const { signup } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState();
+
+  const handleChange = ({ target: { name, value } }) =>
+    setUser({ ...user, [name]: value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(" ");
+    try {
+      await signup(user.email, user.password);
+      navigate("/admon");
+    } catch (error) {
+      console.log(error.code);
+    }
+  };
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      Register
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="ingrese su email"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="ingrese el password"
+          id="password"
+          onChange={handleChange}
+        />
+
+        <button>Registrar</button>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
+export default Register;
--- my-project1/src/routes/Register.js
+++ my-project1/src/routes/Register.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Register
+  const { signup } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState();
+
+  const handleChange = ({ target: { name, value } }) =>
+    setUser({ ...user, [name]: value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(" ");
+    try {
+      await signup(user.email, user.password);
+      navigate("/admon");
+    } catch (error) {
+      console.log(error.code);
+    }
+  };
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      Register
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="ingrese su email"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="ingrese el password"
+          id="password"
+          onChange={handleChange}
+        />
+
+        <button>Registrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;

8d2fb40561f33b100657726ee7baeb4a55c8f3a4
Cristiantorres53 <cristiancamilotorresrodriguez@gmail.com>
2022-05-20 21:23:55 -0500

    logout de usuario

--- project/src/components/NavBar.jsx
+++ project/src/components/NavBar.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../context/UserProvider";
 
 const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleClickLogout = async () => {
+    try {
+      await userSignOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <NavLink to="/">Inicio</NavLink>
-      <NavLink to="/Register">Registro</NavLink>
-      <NavLink to="/admon">Admon</NavLink>
+      {user ? (
+        <div>
+          <NavLink to="/">Inicio</NavLink>{" "}
+          <button onClick={handleClickLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <NavLink to="/Register">Registro</NavLink>
+          <NavLink to="/Admon">Admon</NavLink>
+          <NavLink to="/Login">Login</NavLink>
+        </div>
+      )}
     </div>
   );
 };
--- project/src/context/UserProvider.jsx
+++ project/src/context/UserProvider.jsx
@@ -1,6 +1,9 @@
-import { createUserWithEmailAndPassword } from "@firebase/auth";
-import { signInWithEmailAndPassword } from "@firebase/auth"
-import React, { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "@firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
 import { auth } from "../Firebase";
 
 export const UserContext = createContext();
@@ -8,14 +11,26 @@ export const UserContext = createContext();
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState(false);
 
+  useEffect(() => {
+    const unsuscribe = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+
+    return () => unsuscribe();
+  }, []);
+
   const register = (email, password) =>
     createUserWithEmailAndPassword(auth, email, password);
 
   const login = (email, password) =>
     signInWithEmailAndPassword(auth, email, password);
 
+  const userSignOut = () => signOut(auth);
+
   return (
-    <UserContext.Provider value={{ user, setUser, register, login }}>
+    <UserContext.Provider
+      value={{ user, setUser, register, login, userSignOut }}
+    >
       {children}
     </UserContext.Provider>
   );

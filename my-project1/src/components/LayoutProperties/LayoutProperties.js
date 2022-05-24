e83a8a917fb88358c23442fce0919a7e4947ba04
Cristiantorres53 <cristiancamilotorresrodriguez@gmail.com>
2022-05-23 22:39:10 -0500

    maquetado v1 home

--- my-project1/src/components/LayoutProperties/LayoutProperties.js
+++ my-project1/src/components/LayoutProperties/LayoutProperties.js
@@ -1,74 +1,238 @@
 import React from "react";
-import { Row, Card, Button } from "react-bootstrap";
+import { Row, Card, Button, Image } from "react-bootstrap";
 
 const LayoutProperties = () => {
   return (
     <div>
+
+    <Image src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653360773/reto-final-AG/Text_jksjcc.png"/>
+
       <Row>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <div>
+            <Image
+              variant="top"
+              src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653320897/reto-final-AG/Ellipse_5_zlxym8.png"
+            />
+            <Image src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653321048/reto-final-AG/parking-area_1_qoorku.png" />
+          </div>
+
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-      
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>Parking Speace</Card.Title>
+
+            <Button variant="ligth">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </Button>
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <div>
+            <Image
+              variant="top"
+              src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653320897/reto-final-AG/Ellipse_5_zlxym8.png"
+            />
+            <Image src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653321048/reto-final-AG/parking-area_1_2_ftlwy6.png" />
+          </div>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-      
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>Swimming Pool</Card.Title>
+
+            <Button variant="ligth">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </Button>
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <div>
+            <Image
+              variant="top"
+              src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653320897/reto-final-AG/Ellipse_5_zlxym8.png"
+            />
+            <Image src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653321049/reto-final-AG/parking-area_1_4_lrsd5v.png" />
+          </div>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>Privat Security</Card.Title>
+
+            <Button variant="ligth">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </Button>
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <div>
+            <Image
+              variant="top"
+              src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653320897/reto-final-AG/Ellipse_5_zlxym8.png"
+            />
+            <Image src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653321048/reto-final-AG/parking-area_1_6_xzr6vm.png" />
+          </div>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>Medical Center</Card.Title>
+
+            <Button variant="ligth">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </Button>
           </Card.Body>
         </Card>
       </Row>
       <Row>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <div>
+            <Image
+              variant="top"
+              src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653320897/reto-final-AG/Ellipse_5_zlxym8.png"
+            />
+            <Image src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653321048/reto-final-AG/parking-area_1_1_dc4dge.png" />
+          </div>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>Library Area</Card.Title>
+
+            <Button variant="ligth">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </Button>
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <div>
+            <Image
+              variant="top"
+              src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653320897/reto-final-AG/Ellipse_5_zlxym8.png"
+            />
+            <Image src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653321048/reto-final-AG/parking-area_1_3_blmujg.png" />
+          </div>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>King Size Beds</Card.Title>
+
+            <Button variant="ligth">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </Button>
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <div>
+            <Image
+              variant="top"
+              src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653320897/reto-final-AG/Ellipse_5_zlxym8.png"
+            />
+            <Image src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653321048/reto-final-AG/parking-area_1_5_pafv1m.png" />
+          </div>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>Smart Homes</Card.Title>
+
+            <Button variant="ligth">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </Button>
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+          <div>
+            <Image
+              variant="top"
+              src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653320897/reto-final-AG/Ellipse_5_zlxym8.png"
+            />
+            <Image src="https://res.cloudinary.com/dorkyjlu8/image/upload/v1653321048/reto-final-AG/parking-area_1_7_znmyuz.png" />
+          </div>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            
-            <Button variant="primary">Go somewhere</Button>
+            <Card.Title>Kid's Playland</Card.Title>
+
+            <Button variant="ligth">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrow-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </Button>
           </Card.Body>
         </Card>
       </Row>
